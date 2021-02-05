de703342ced0b6ac1a05fa617db321a442653c2b
Awais Shaikh <awaisshaikh.official@gmail.com>
2021-02-05 15:34:10 +0500

    array push learn

---
+++ array.js
@@ -0,0 +1,15 @@
+
+var bikes = ["honda", "unique", "yamaha", "star"];
+
+console.log(bikes);
+
+// array methods
+console.log(bikes.toString());
+
+
+// for (i = 0; i < bikes.length; i++) {
+//     console.log(bikes[i]);
+// } 
+
+// console.log(bikes[0]);
+
---
+++ custom.js
@@ -0,0 +1,5 @@
+function testName() {
+    console.log('name test');
+}
+
+testName();
